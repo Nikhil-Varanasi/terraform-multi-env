8cb8976fdbb23d3186e3a05279782a9ea5521fe4
Nikhil-Varanasi <nikhilvaranasi111@gmail.com>
2024-12-10 19:50:30 +0530

    workspaces added

---
+++ workspaces/ec2.tf
@@ -0,0 +1,8 @@
+resource "aws_instance" "terraform" {
+    ami = "ami-09c813fb71547fc4f"
+    vpc_security_group_ids = ["sg-0996d6bc9e1352780"]
+    instance_type = lookup(var.instance_type, terraform.workspace)
+    tags = {
+        Name = "terraform"
+    }
+}
